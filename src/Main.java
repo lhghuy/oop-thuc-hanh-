272f58eb1bf77755d139bb57af71e3ae70c774ce
Gia Huy <lhghuy@gmail.com>
2024-03-19 09:37:53 +0700

    HangHoa

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        DienMay dm = new DienMay();
+        SanhSu ss = new SanhSu();
+        ThucPham tp = new ThucPham();
+        System.out.println("\ndien may ");
+        dm.dmm();
+        System.out.println("\nthuc pham ");
+        tp.tpp();
+        System.out.println("\nsanh su ");
+        ss.sss();
+    }
+}
